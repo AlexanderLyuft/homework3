b801b29587a3a5fac36d2e8093a04eacf33f7516
AlexanderLyuft <alexanderlyuft@mail.ru>
2024-06-30 07:06:48 +0700

    Update homework3.py

--- homework3.py
+++ homework3.py
@@ -2,7 +2,6 @@ name = 'Alex'
 print('Name:', name)
 age = 32
 print('Age:', age)
-new_age = 33
-print('new_age:', new_age)
+print('new_age:', age + 1)
 is_student = True
 print('Is student:', is_student)
